4c5de119ae8c0bc80e69afa2aaf03b7cfb719999
safakun <safarovdeem@gmail.com>
2023-12-07 12:45:13 +0500

    changed some shit

--- src/app/add_product/page.tsx
+++ src/app/add_product/page.tsx
@@ -4,8 +4,41 @@ return (
         <h1 className="text-lg mb-3 font-bold">Add Product</h1>
         <form>
             <input
+            required
+            name="name"
+            placeholder="Name"
             className="mb-3 w-full input input-bordered"
             type="text" />
+
+            <textarea
+            required
+            name="description" 
+            placeholder="Description"
+            className="textarea textarea-bordered mb-3 w-full"
+            />
+
+            <input
+            required
+            name="imageUrl"
+            placeholder="image URL"
+            className="mb-3 w-full input input-bordered"
+            type="url" />
+
+            <input
+            required
+            name="price"
+            placeholder="Price"
+            className="mb-3 w-full input input-bordered"
+            type="text" />
+
+            <button
+            type="submit"
+            className="btn btn-primary btn-block"
+            >
+                Add Product
+            </button>
+
+          
         </form>
     </div>
 )
