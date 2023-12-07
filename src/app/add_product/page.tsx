3d5181719e5233cb6de17c1a73a30556f9529ce6
safakun <safarovdeem@gmail.com>
2023-12-07 12:18:25 +0500

    added component to add

---
+++ src/app/add_product/page.tsx
@@ -0,0 +1,12 @@
+export default function AddProductPage() {
+return (
+    <div>
+        <h1 className="text-lg mb-3 font-bold">Add Product</h1>
+        <form>
+            <input
+            className="mb-3 w-full input input-bordered"
+            type="text" />
+        </form>
+    </div>
+)
+}
